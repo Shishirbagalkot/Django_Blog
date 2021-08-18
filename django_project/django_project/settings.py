7594630d5cb62bc6460840db862715ffdbe6bab8
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2021-08-18 23:55:13 +0530

    Setting.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
