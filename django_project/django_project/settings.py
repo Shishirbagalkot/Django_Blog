cf003279664d6c557dc12e51c0f0ea0cd04c0eeb
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2021-08-19 01:35:37 +0530

    Profile pic

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-$non()msef35!b*zhp*cctun^j7m&9@bnozz=*i*p*ub4%a)$%'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == True)
+# DEBUG = (os.environ.get('DEBUG_VALUE') == True)
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-tech-blog.herokuapp.com']
 
